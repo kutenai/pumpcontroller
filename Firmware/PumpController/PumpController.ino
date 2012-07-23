5dd479ac6f0da606da56ebbf2acbf61cdcd49ea3
Ed Henderson <ed@eeweb.com>
2012-07-22 20:49:10 -0600

    Latest and greatest..

--- Firmware/PumpController/PumpController.ino
+++ Firmware/PumpController/PumpController.ino
@@ -7,13 +7,34 @@
 char field_separator = ',';
 char command_separator = ';';
 
-int sensor1Curr = 0;
-long sensor1Sum = 0;
-int sensor1Count = 0;
+int pumpPin = 2;
+int northPin = 3;
+int southPin = 4;
 
-int sensor2Curr = 0;
-long sensor2Sum = 0;
-int sensor2Count = 0;
+// Analog inputs.
+int ditchPin = 2;
+int sumpPin = 5;
+
+// Set the pump call on when we want the pump to go..
+// we might turn it off if the levels are too low.
+bool pumpCall = false;
+
+// Set these to true when we want the valve on. The actual state of
+// the valve will be off unless the pump is on.. 
+bool northCall = false;
+bool southCall = false;
+
+int ditchCurr = 0;
+long ditchSum = 0;
+int ditchCount = 0;
+
+int sumpCurr = 0;
+long sumpSum = 0;
+int sumpCount = 0;
+
+// Trigger levels.. to turn off the pump if the levels are reached
+int sumpLowTrigger = 0;
+bool enableSumpTrigger = false;
 
 // Attach a new CmdMessenger object to the default Serial port
 CmdMessenger cmdMessenger = CmdMessenger(Serial, field_separator, command_separator);
@@ -46,33 +67,66 @@ enum
 messengerCallbackFunction messengerCallbacks[] = 
 {
   enablepump_msg,            // 004 in this example
-  valve1_msg,
-  valve2_msg,
-  sensordata_msg,  
+  north_msg,
+  south_msg,
+  status_msg,
+  sumptrigger_msg,
+  sumptrigger_en_msg,
+  setAnalogPins_msg,  
   NULL
 };
 
+bool isPumpOn() {
+  int pin = digitalRead(pumpPin);
+  if (pin == LOW) {
+    return true;
+  }
+  return false;
+}
+bool isNorthOn() {
+  int pin = digitalRead(northPin);
+  if (pin == LOW) {
+    return true;
+  }
+  return false;
+}
+bool isSouthOn() {
+  int pin = digitalRead(southPin);
+  if (pin == LOW) {
+    return true;
+  }
+  return false;
+}
+
 void setPump(bool bOn) {
   if (bOn) {
-    digitalWrite(2,LOW);
+    digitalWrite(pumpPin,LOW);
   } else {
-    digitalWrite(2,HIGH);
+    digitalWrite(pumpPin,HIGH);
   }
+}
+
+void setNorthCall(bool bOn) {
+  northCall = bOn;
 }
 
 void setNorthValve(bool bOn) {
   if (bOn) {
-    digitalWrite(3,LOW);
+    digitalWrite(northPin,LOW);
   } else {
-    digitalWrite(3,HIGH);
+    digitalWrite(northPin,HIGH);
   }
+}
+
+void setSouthCall(bool bOn) {
+  southCall = bOn;
 }
 
 void setSouthValve(bool bOn) {
   if (bOn) {
-    digitalWrite(4,LOW);
+    digitalWrite(southPin,LOW);
   } else {
-    digitalWrite(4,HIGH);
+    digitalWrite(southPin,HIGH);
   }
 }
 
@@ -82,101 +136,215 @@ void enablepump_msg()
 {
   // Message data is any ASCII bytes (0-255 value). But can't contain the field
   // separator, command separator chars you decide (eg ',' and ';')
-  cmdMessenger.sendCmd(kACK,"enable pump recieved");
-  setFlash(6);
+  //cmdMessenger.sendCmd(kACK,"enable pump recieved");
+  //setFlash(6);
 
   while ( cmdMessenger.available() )
   {
     char buf[350] = { '\0' };
     cmdMessenger.copyString(buf, 350);
     if(buf[0]) {
-      cmdMessenger.sendCmd(kACK, buf);
       if (strcmp(buf,"on") == 0) {
-        cmdMessenger.sendCmd(kACK, "PumpOn");
-        setPump(true);
+        cmdMessenger.sendCmd(kACK, "Ok:PumpOn");
+        pumpCall = true;
+      } else if (strcmp(buf,"off") == 0) {
+        cmdMessenger.sendCmd(kACK, "Ok:PumpOff");
+        pumpCall = false;
       } else {
-        cmdMessenger.sendCmd(kACK, "PumpOff");
-        setPump(false);
+        char msg[30];
+        sprintf(msg,"Fail:invalid command:%s",buf);
+        cmdMessenger.sendCmd(kERR, msg);
       }
     }
   }
 }
 
-void valve1_msg() {
+void north_msg() {
   // Message data is any ASCII bytes (0-255 value). But can't contain the field
   // separator, command separator chars you decide (eg ',' and ';')
-  setFlash(5);
-  cmdMessenger.sendCmd(kACK,"valve1 msg received");
+  //setFlash(5);
+  //cmdMessenger.sendCmd(kACK,"valve1 msg received");
   while ( cmdMessenger.available() )
   {
     char buf[350] = { '\0' };
     cmdMessenger.copyString(buf, 350);
     if(buf[0]) {
-      cmdMessenger.sendCmd(kACK, buf);
+      //cmdMessenger.sendCmd(kACK, buf);
       if (strcmp(buf,"on") == 0) {
-        setNorthValve(true);
+        setNorthCall(true);
+        cmdMessenger.sendCmd(kACK,"Ok");
       } else {
-        setNorthValve(false);
+        setNorthCall(false);
+        cmdMessenger.sendCmd(kACK,"Ok");
       }
     }
   }
 }
 
-void valve2_msg() {
+void south_msg() {
   // Message data is any ASCII bytes (0-255 value). But can't contain the field
   // separator, command separator chars you decide (eg ',' and ';')
-  setFlash(4);
-  cmdMessenger.sendCmd(kACK,"valve2 msg received");
-  while ( cmdMessenger.available() )
-  {
+  //setFlash(4);
+  //cmdMessenger.sendCmd(kACK,"valve2 msg received");
+  while ( cmdMessenger.available() ) {
     char buf[350] = { '\0' };
     cmdMessenger.copyString(buf, 350);
     if(buf[0]) {
       cmdMessenger.sendCmd(kACK, buf);
       if (strcmp(buf,"on") == 0) {
-        setSouthValve(true);
+        setSouthCall(true);
       } else {
-        setSouthValve(false);
+        setSouthCall(false);
       }
     }
   }
 }
 
-void sensordata_msg() {
+void status_msg() {
   char buf[350] = { '\0' };
   
   // Read sensors.
-  sprintf(buf,"%d,%d",sensor1Curr,sensor2Curr);
+  sprintf(buf,"D:%d S:%d PC:%d P:%d NC:%d N:%d SC%d: S:%d ST:%d STen:%d",
+    ditchCurr,
+    sumpCurr,
+    pumpCall,
+    isPumpOn(),
+    northCall, isNorthOn(),
+    southCall, isSouthOn(),
+    sumpLowTrigger,
+    enableSumpTrigger
+    );
   cmdMessenger.sendCmd(kACK,buf);
 
-  setFlash(3);
+  //setFlash(3);
+}
+
+void sumptrigger_msg() {
+  while ( cmdMessenger.available() )
+  {
+    char buf[350] = { '\0' };
+    cmdMessenger.copyString(buf, 350);
+    if(buf[0]) {
+      int triggerLevel = atoi(buf);
+
+      if (triggerLevel == 0 || triggerLevel > 1023) {
+        // Probably a bad value
+        char msg[30];
+        sprintf(msg,"Fail:bad trigger level(%s), enter a number between 1 and 1023",buf);
+        cmdMessenger.sendCmd(kERR, msg);
+      } else {
+        sumpLowTrigger = triggerLevel;
+        char msg[30];
+        sprintf(msg,"Ok:sump trigger to set %d",sumpLowTrigger);
+        cmdMessenger.sendCmd(kACK, msg);
+      }
+    }
+  }
+}
+
+void sumptrigger_en_msg() {
+  while ( cmdMessenger.available() )
+  {
+    char buf[350] = { '\0' };
+    cmdMessenger.copyString(buf, 350);
+    if(buf[0]) {
+      if (strcmp(buf,"en") == 0) {
+        enableSumpTrigger = true;
+        cmdMessenger.sendCmd(kACK, "Ok:Sump Trigger Enabled.");
+      } else if (strcmp(buf,"disable") == 0)  {
+        enableSumpTrigger = false;
+        cmdMessenger.sendCmd(kACK, "Ok:Sump Trigger Disabled.");
+      } else {
+        char msg[30];
+        sprintf(msg,"Fail:invalid command:%s use one of en,disable",buf);
+        cmdMessenger.sendCmd(kERR, msg);
+      }
+    }
+  }
+}
+
+// Pass 2 values, pin for ditch, and pin for sump.
+// The defaults are 2 and 5.. but you can swap these to
+// 5 and 2.. 
+void setAnalogPins_msg() {
+  if ( cmdMessenger.available() )
+  {
+    // First is the ditch number..
+    char buf[350] = { '\0' };
+    cmdMessenger.copyString(buf, 350);
+    if(buf[0]) {
+      int pinNum = atoi(buf);
+
+      if (pinNum == 2 || pinNum == 5) {
+        if (pinNum == 2) {
+          ditchPin = 2;
+          sumpPin = 5;
+        } else {
+          ditchPin = 5;
+          sumpPin = 2;
+        }
+        char msg[30];
+        sprintf(msg,"Ok:ditchPin:%d sumpPin:%d",ditchPin,sumpPin);
+        cmdMessenger.sendCmd(kERR, msg);
+      } else {
+        char msg[30];
+        sprintf(msg,"Fail:please specify the ditch pin as 2 or 5");
+        cmdMessenger.sendCmd(kERR, msg);
+      }
+    }
+    while (cmdMessenger.available()) ;
+  }
+}
+
+void levelChecks() {
+    if (enableSumpTrigger && sumpCurr < sumpLowTrigger) {
+      setPump(false);
+    } else {
+      setPump(pumpCall);
+    }
+}
+/*
+  Called once a second. 
+  If the pump is off, then force both valves off.
+*/
+void updateValves() {
+  if (isPumpOn() ) {
+    setNorthValve(northCall);
+    setSouthValve(southCall);
+  } else {
+    // Always turn valves off if the pump
+    // is off.
+    setNorthValve(false);
+    setSouthValve(false);
+  }
 }
 
 /*
-  Called periodically..
+  Called often.. reads sensors and applies hardware
+  averaging.
 */
 void readSensors() {
-  int sensor1 = analogRead(2);
-  int sensor2 = analogRead(5);
+  int ditch = analogRead(ditchPin);
+  int sump = analogRead(sumpPin);
   
-  sensor1Sum += sensor1;
-  sensor1Count++;
-  if (sensor1Count > 100) {
-    double sensor1D = sensor1Sum;
-    sensor1D /= sensor1Count;
-    sensor1Curr = (int)sensor1D;
-    sensor1Count = 0;
-    sensor1Sum = 0;
+  ditchSum += ditch;
+  ditchCount++;
+  if (ditchCount >= 1000) {
+    double ditchD = ditchSum;
+    ditchD /= (double)ditchCount;
+    ditchCurr = (int)ditchD;
+    ditchCount = 0;
+    ditchSum = 0;
   }
 
-  sensor2Sum += sensor2;
-  sensor2Count++;
-  if (sensor1Count > 100) {
-    double sensor2D = sensor2Sum;
-    sensor2D /= sensor2Count;
-    sensor2Curr = (int)sensor2D;
-    sensor2Count = 0;
-    sensor2Sum = 0;
+  sumpSum += sump;
+  sumpCount++;
+  if (sumpCount >= 1000) {
+    double sumpD = sumpSum;
+    sumpD /= (double)sumpCount;
+    sumpCurr = (int)sumpD;
+    sumpCount = 0;
+    sumpSum = 0;
   }
 }
 
@@ -239,12 +407,10 @@ void setup() {
 // ------------------ M A I N ( ) --------------------------------------------
 
 // Timeout handling
-long timeoutInterval = 2000; // 2 seconds
-long previousMillis = 0;
+long oneSecondInterval = 1000;
+long oneSecondCounter = 0;
+
 int counter = 0;
-int flashCount = 0;
-long flashInterval = 200;
-long previousFlashMillis = 0;
 
 void timeout()
 {
@@ -254,43 +420,25 @@ void timeout()
   } else {
     digitalWrite(13, LOW);
   }
-  counter ++;
+  counter++;
 }  
-
-void setFlash(int n) {
-  counter = 0;
-  digitalWrite(13, LOW);
-  previousFlashMillis = millis();
-  flashCount = n;
-}
 
 void loop() 
 {
   // Process incoming serial data, if any
   cmdMessenger.feedinSerialData();
 
-  if (flashCount) {
-    previousMillis = millis();
-    // handle timeout function, if any
-    if (  millis() - previousFlashMillis > flashInterval )
-    {
-      timeout();
-      previousFlashMillis = millis();
-      if (counter % 2 == 1) {
-        flashCount--;
-      }
-    }
-  } else {
-    
-    // handle timeout function, if any
-    if (  millis() - previousMillis > timeoutInterval )
-    {
-      timeout();
-      previousMillis = millis();
-    }
+  // Things to do all the time.
+  readSensors();
+
+  if ( millis() - oneSecondCounter > oneSecondInterval) {
+    oneSecondCounter = millis();
+    // Things to do at a one-second interval. 
+    timeout();
+    levelChecks();
+    updateValves();
   }
 
-  // Loop.
 }
 
 
