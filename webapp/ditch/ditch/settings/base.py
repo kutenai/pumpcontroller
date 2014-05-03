fc19f832dec2f8f6dc9b18ea6735066fbce18e9a
Ed Henderson <ed@eeweb.com>
2014-05-02 21:40:38 -0600

    More fixes to base/index files.

--- webapp/ditch/ditch/settings/base.py
+++ webapp/ditch/ditch/settings/base.py
@@ -12,10 +12,6 @@ DJANGO_ROOT = Path(__file__).ancestor(3)
 SITE_ROOT = DJANGO_ROOT.ancestor(2)
 SCRIPT_ROOT = SITE_ROOT.child('scripts')
 
-SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
-APPLICATION_PATH = os.path.join(os.path.normpath(os.path.dirname(__file__)),'../..')
-STATIC_DIR = os.path.normpath(os.path.join(APPLICATION_PATH,'../static'))
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -62,18 +58,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = SITE_ROOT.child('site_media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/site_media/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = SITE_ROOT.child('collectedstatic')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -84,7 +80,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    STATIC_DIR,
+    DJANGO_ROOT.child('static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -140,7 +136,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(APPLICATION_PATH,'templates'),
+    DJANGO_ROOT.child('templates'),
 )
 
 INSTALLED_APPS = (
