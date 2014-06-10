3f373a9c4860657db614c14f06c5df0a6d8c7901
Ed Henderson <ed@eeweb.com>
2014-06-10 12:18:59 -0600

    Sweet.. I finally got a chain working!!! Excellent..

--- celery/test_all.py
+++ celery/test_all.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
+from celery import chain
+
 from dbtasks.tasks import onstatus
 from ditchtasks.tasks import status
 
 if __name__ == "__main__":
 
     print("Name:%s" % status.name)
+    print("OnStatus:%s" % onstatus.name)
+
+    ch = chain(status.s() | onstatus.s())
+
+    result = ch.apply_async()
 
     result = status.delay()
 
     print("Result:%s" % result)
-
-    s = result.get(timeout=10)
-    print("Status:%s" % s)
-
-    onstatus.delay(s)
 
